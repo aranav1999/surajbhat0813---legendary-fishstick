d49f4ee42b0fa04f67ef0e7f3c4b282cf075d4dc
aranav1999 <aranav1999@gmail.com>
2023-08-31 14:51:45 +0530

    Form adittion

---
+++ src/app/Test/page.js
@@ -0,0 +1,60 @@
+"use client"
+
+import React from 'react'
+import { useState } from 'react'
+
+const Quiz = () => {
+    const [answer, setAnswer] = useState('');
+    const [status, setStatus] = useState('Typing...');
+    const [error, setError] = useState(null);
+
+    if (status === 'Success') {
+        return <h1>That's right!</h1>
+      }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setStatus('Submitting');
+        try {
+            await CheckAnswer(answer);
+            setStatus("Success");
+        } catch (error) {
+            setStatus("Typing...");
+            setError(error);
+        }
+    }
+
+
+  return (
+    <>
+    <h1>City Quiz</h1>
+    <p>In which city is there a billboard that turns air into drinkable water?</p>
+    <form onSubmit={handleSubmit}>
+        <br/>
+        <textarea className="border border-grey-900 rounded-xl" onChange={(e) => {setAnswer(e.target.value)}}/>
+        <br/>
+        <button className='border border-black-900 hover:bg-slate-200' disabled={answer.length===0 || status === "Submitting"}>Submit</button>
+        {error !== null &&
+          <p className="Error">
+            {error.message}
+          </p>
+        }
+    </form>
+    </>
+  )
+}
+
+function CheckAnswer(answer) {
+    // Pretend it's hitting the network.
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let shouldError = answer.toLowerCase() !== 'lima'
+        if (shouldError) {
+          reject(new Error('Good guess but a wrong answer. Try again!'));
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+export default Quiz
