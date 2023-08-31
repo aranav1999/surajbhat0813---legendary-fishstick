fc891b1db758e039ab24b9ca65ef4b661e18fb64
surajbhat0813 <bhatsuraj22@gmail.com>
2023-08-31 17:43:27 +0530

    added styling

--- src/app/Test/page.js
+++ src/app/Test/page.js
@@ -4,61 +4,63 @@ import React from 'react'
 import { useState } from 'react'
 
 const Quiz = () => {
-    const [answer, setAnswer] = useState('');
-    const [status, setStatus] = useState('Typing...');
-    const [error, setError] = useState(null);
+  const [answer, setAnswer] = useState('');
+  const [status, setStatus] = useState('Typing...');
+  const [error, setError] = useState(null);
 
-    if (status === 'Success') {
-        return <h1>That's right!</h1>
-      }
+  if (status === 'Success') {
+    return <h1>That's right!</h1>
+  }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setStatus('Submitting');
-        try {
-            await CheckAnswer(answer);
-            setStatus("Success");
-        } catch (error) {
-            setStatus("Typing...");
-            setError(error);
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus('Submitting');
+    try {
+      await CheckAnswer(answer);
+      setStatus("Success");
+    } catch (error) {
+      setStatus("Typing...");
+      setError(error);
     }
+  }
 
 
   return (
     <>
-    
-<div className='w-1/3 border rounded-xl mx-auto p-6 bg-sky-100'>
+      <div className='bg-gradient-to-b from-sky-400 to-white flex items-center justify-center h-screen'>
 
-    <h1 className='font-bold text-cyan-700 italic text-xl'> City Quiz</h1>
-    <p className='italic'>  In which city is there a billboard that turns air into drinkable water?</p>
-    <form onSubmit={handleSubmit}>
-        <br/>
-        <textarea className="border border-grey-900 rounded-xl" onChange={(e) => {setAnswer(e.target.value)}}/>
-        <br/>
-        <button className='transition ease-in-out delay-100 bg-blue-500 text-white font-bold hover:scale-110 hover:bg-indigo-500 duration-100 border rounded-xl px-3 py-1 active:border-green-900 ' disabled={answer.length===0 || status === "Submitting"}>Submit</button>
-        {error !== null &&
-          <p className="Error">
-            {error.message}
-          </p>
-        }
-    </form>
-</div>
+        <div className='flex flex-col drop-shadow-2xl w-1/3 h-1/3 border rounded-xl mx-auto p-6 bg-sky-200 my-50'>
+
+          <h1 className='font-bold text-cyan-700 italic text-xl'> City Quiz</h1>
+          <p className='italic'>  In which city is there a billboard that turns air into drinkable water?</p>
+          <form onSubmit={handleSubmit}>
+            <br />
+            <textarea className=" w-full h-full border border-grey-900 rounded-xl px-2 py-1" onChange={(e) => { setAnswer(e.target.value) }} />
+            <br />
+            <button className=' m-2 transition ease-in-out delay-100 bg-blue-500 text-white font-bold hover:scale-110 hover:bg-indigo-500 duration-100 border rounded-xl px-3 py-1 active:border-green-900 ' disabled={answer.length === 0 || status === "Submitting"}>Submit</button>
+            {error !== null &&
+              <p className="Error text-red-500">
+                {error.message}
+              </p>
+            }
+          </form>
+        </div>
+      </div>
     </>
   )
 }
 
 function CheckAnswer(answer) {
-    // Pretend it's hitting the network.
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        let shouldError = answer.toLowerCase() !== 'lima'
-        if (shouldError) {
-          reject(new Error('Good guess but a wrong answer. Try again!'));
-        } else {
-          resolve();
-        }
-      });
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== 'lima'
+      if (shouldError) {
+        reject(new Error('Good guess but a wrong answer. Try again!'));
+      } else {
+        resolve();
+      }
     });
-  }
+  });
+}
 export default Quiz
